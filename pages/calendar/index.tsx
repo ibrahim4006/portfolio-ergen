9ff26e2e54da69ce0e1a31246b424c3e36817283
ergen4006 <brhmrgn1@gmail.com>
2024-03-28 11:02:46 +0300

    try

--- pages/calendar/index.tsx
+++ pages/calendar/index.tsx
@@ -6,7 +6,7 @@ import Calendar from "@/components/Sections/RecentWorks/Calendar";
 
 const index = () => {
   return (
-    <div className="kursu-outer-lines min-h-screen flex flex-col w-full relative justify-between items-center bg-[#0d0d0d]">
+    <div className="kursu-outer-lines min-h-screen flex flex-col w-full relative justify-between items-center bg-[#0d0d0d] ">
       <Corners />
       <CenterTop />
       <Calendar /> <CenterBottom />
